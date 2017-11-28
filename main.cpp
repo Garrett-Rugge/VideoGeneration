d60f95f55e3931e227d4eeee9ef441abcc8985f2
Garrett <garrett.rugge@gmail.com>
2017-11-27 16:14:41 -0800

    x

--- main.cpp
+++ main.cpp
@@ -33,30 +33,48 @@
 unsigned char frame[H][W][3];
 const double frames_per_second = 30; 
 const int duration_in_seconds = 3;
+const double pps = 80;
 
 typedef unsigned char byte;
 class Rectangle{
 	public:
-		Rectangle(){ }
+		Rectangle();
+		Rectangle(int w, int h, byte r, byte g, byte b);
 		void clamp(int * x, int * y);
 		void clear_frame() { memset(frame, 0, sizeof(frame)); }
-		void draw_rect(int x, int y, int w, int h, byte r, byte g, byte b);
+		void draw_rect();
 		void draw_frame(double t);
 		bool outside_frame(int * x, int * y);	
+	private:
+		int x, y, w, h;
+		byte r, g, b;
 };
 
 using namespace std;
 
+Rectangle::Rectangle(){
+	x = 0;
+	y = 0;
+	w = 0;
+	h = 0;
+	r = 0x00;
+	g = 0x00;
+	b = 0x00;
+}
+Rectangle::Rectangle(int w, int h, byte r, byte g, byte b){
+	w = w;
+	h = h;
+	r = r;
+	g = g;
+	b = b;
+}
 // Main drawing code.
 // Expand this function to add content to the video.
 void Rectangle::draw_frame(double t) {
 	clear_frame();
-	const double pps = 80; // pixels per second
-	draw_rect(0 + t * pps, 0 + t * pps, 20, 10, 0x00, 0xff, 0x00);
-	draw_rect(150 + t * pps, 150 + t * pps, 50, 100, 0x00, 0x00, 0xff);
-	draw_rect(-50 + t * pps, -50 + t * pps, 10, 5, 0xff, 0x00, 0x00);
-	draw_rect(100 + t * pps, 100 + t * pps, 2, 4, 0xff, 0xff, 0xff);
-	draw_rect(-100 + t * pps, -100 + t * pps, 15, 50, 0xff, 0xff, 0x00);
+	x = 0 + t * pps;
+	y = 0 + t * pps;
+	draw_rect();
 }
 
 // Constrain point to frame.
@@ -71,7 +89,7 @@ bool Rectangle::outside_frame(int * x, int * y) {
 
 // Draw a solid rectangle at given location, with given width and height
 // and with given RGB color value.
-void Rectangle::draw_rect(int x, int y, int w, int h, byte r, byte g, byte b) {
+void Rectangle::draw_rect() {
 	if (outside_frame(&x, &y)) return;
 	int x0 = x;
 	int x1 = x + w;
@@ -91,7 +109,6 @@ void Rectangle::draw_rect(int x, int y, int w, int h, byte r, byte g, byte b) {
 
 int main(int argc, char * argv[]) {
 	// Construct the ffmpeg command to run.
-	Rectangle r;
 	const char * cmd = 
 		"ffmpeg              "
 		"-y                  "
@@ -108,6 +125,7 @@ int main(int argc, char * argv[]) {
 		"output.mp4          ";
 
 	// Run the ffmpeg command and get pipe to write into its standard input stream.
+	Rectangle r(20, 10, 0x00, 0xff, 0x00);
 	FILE * pipe = popen(cmd, "w");
 	if (pipe == 0) {
 		cout << "error: " << strerror(errno) << endl;
